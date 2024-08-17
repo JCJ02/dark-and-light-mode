9693df60f6d9b3dcd1cfe66eed73a5074742ddc8
JCJ02 <jacobe.johncarlo.02022003@gmail.com>
2024-08-18 04:42:21 +0800

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-[#262626] dark:bg-[#FFFFFF] flex flex-col items-center py-[1rem] w-full">
-        <label className="font-mono font-bold text-lg text-[#FFFFFF] dark:text-[#262626]">John Carlo P. Jacobe</label>
-        <label className="font-mono text-xs text-[#FFFFFF] dark:text-[#262626]">&copy; Copyright 2024. ALL RIGHTS RESERVED.</label> 
+      <label className="font-mono font-bold text-lg text-[#FFFFFF] dark:text-[#262626]">John Carlo P. Jacobe</label>
+      <label className="font-mono text-xs text-[#FFFFFF] dark:text-[#262626]">Copyright &copy; 2024 | ALL RIGHTS RESERVED.</label> 
     </footer>
   )
 }
